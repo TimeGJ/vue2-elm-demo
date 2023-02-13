9bd4c07ccebdca6ae4bab9e93f6d9a8911b94ec9
gongjunjun <706707301@qq.com>
2023-02-13 17:44:44 +0800

    shop

--- src/page/shop/shop.vue
+++ src/page/shop/shop.vue
@@ -30,30 +30,29 @@
                 <div  @touchend="changeShowbd"><span :class="{bd_b:!changeShow}">评价</span></div>
             </nav>
         <section v-show="changeShow" class="food_left">
-                <div>
+                <div class="left_food_contain">
                     <section class="food_list">
-                        <ul>
-                            <li v-for="item,index in foodList" :key="item.id">
+                        <ul :style="{transform:'translateY('+mousemove+'px)'}" @touchstart="translate"  @touchmove="transformY" @touchend="transEnd">
+                            <li v-for="item,index in foodList" :key="item.id" @touchend="fooditem(index)" :style="index==foodindex ?'background-color: #fff;':'background-color: #f8f8f8;'">
                                 <span>{{item.name}}</span>
                             </li>
                         </ul>
                     </section>
-                    <section>
+                    <section class="foods">
                         <section>
                             <div>
-                                
+                                {{foodList[foodindex].name}} {{foodList[foodindex].description}}
                             </div>
                             <ul>
                                 <li v-for="item in food[foodindex]">
                                     <div>
-                                        <img src="" alt="">
+                                        <img :src="baseImg+item.image_path" alt="">
                                     </div>
                                     <div>
-                                        <div><span></span><span></span></div>
-                                        <div></div>
-                                        <div></div>
-                                        <div></div>
-                                        <div><span></span> <div><svg><use></use></svg></div></div>
+                                        <div>{{item.name}}</div>
+                                        <div>{{item.description}}</div>
+                                        <div><span>月售{{item.month_sales}}</span><span>好评率{{item.month_sales}}</span></div>
+                                        <div><span class="font_color">￥{{item.specfoods[0].price}}</span> <div><svg><use xlink:href="#icon-add"></use></svg></div></div>
                                     </div>
                                 </li>
                             </ul>
@@ -61,7 +60,22 @@
                     </section>
                 </div>
                 <footer class="footer_box">
-
+                    <section>
+                        <div>
+                            <svg>
+                                <use xlink:href="#icon-shopping-cart-"></use>
+                            </svg>
+                            <div>
+                                <p>
+                                    ￥{{price}}
+                                </p>
+                                <p>
+                                    配送费{{data.float_delivery_fee}}
+                              </p>
+                            </div> 
+                        </div>
+                        <div class="food_price"><p>还差{{data.float_minimum_order_amount}}起送</p></div>
+                    </section>
                 </footer>
         </section>
         <section  v-show="!changeShow"></section>
@@ -79,6 +93,10 @@ export default{
             foodList:[],//食品列表
             food:[],//食物
             foodindex:0,//食物对应索引
+            mouseY:null,//鼠标的位置
+            mousemove:0,//鼠标移动的位置
+            listStare:0,//列表初始位置
+            price:0.00,//价格
         }
     },
     methods:{
@@ -114,8 +132,27 @@ export default{
                 console.log(error)
             })
         },
+        //获取鼠标的位置
+        translate(e){
+            this.mouseY=e.touches[0].clientY
+        },
+        //获取鼠标移动的距离
+        transformY(e){
+            this.mousemove=this.listStare+e.touches[0].clientY-this.mouseY
+        },
+        //移动结束时
+        transEnd(){
+            this.listStare=this.mousemove
+            if(this.listStare>0){
+                this.mousemove=0
+                this.listStare=this.mousemove
+            }
+           
+        },
         //获取对应的食物列表
-
+        fooditem(index){
+            this.foodindex=index
+        }
     },
     created(){
         this.initdata()
@@ -126,6 +163,12 @@ export default{
 </script>
 <style lang="less" scoped>
 .shop_box{
+    position: absolute;
+    right: 0;
+    left: 0;
+    display: flex;
+    flex-direction: column;
+    height: 100%;
     .heard_box{
       position: relative;
         width: 100%;
@@ -214,14 +257,20 @@ export default{
             }
         }
         .food_left{
-            width: 5rem;
-            div{
+            display: flex;
+            flex: 1;
+            overflow: hidden;
+            padding-bottom: 2rem;
+            
+            .left_food_contain{
+            display: flex;
+            flex: 1;
           .food_list{
+            height: 100%;
+            width: 5rem;
                 ul{
-                    height: 26rem;
-                    overflow: hidden;
-                    .choose_bd{
-                        border-left: 2px solid #3190e8;
+                    transition: all 0ms;
+                    .bgc{
                         background-color: #fff;
                     }
                    li{
@@ -235,6 +284,104 @@ export default{
                     }
                    }
 
+                }
+            }
+        .foods{
+            overflow-y: auto;
+            width: 75%;
+            section{
+          ul{
+            li{
+                display: flex;
+                justify-content: start;
+                background-color: #fff;
+                padding: 0.5rem;
+                border-bottom: 1px solid #f8f8f8;
+                div:nth-child(1){
+                    img{
+                        margin: 0.5rem;
+                        width: 2.5rem;
+                        height: 2.5rem;
+                    }
+                }
+                div:nth-child(2){
+                    width: 100%;
+                    div:nth-child(1){
+                        font-weight: 700;
+                        color: #333;
+                    }
+                    div:nth-child(2){
+                        font-size: 0.5rem;
+                        color: #999;
+                    }
+                    div:nth-child(3){
+                        font-size: 0.4rem;
+                    }
+                    div:nth-child(4){
+                        display: flex;
+                        justify-content: space-between;
+                        .font_color{
+                            color: #f60;
+                        }
+                        div{
+                            display: flex;
+                            justify-content: end;
+                            svg{
+                                width: 1.5rem;
+                                height: 1.5rem;
+                            }
+                        }
+                    }
+                }
+            }
+          }
+        }
+    }
+        }
+        .footer_box{
+            position: fixed;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            height: 2.5rem;
+            background-color: #3d3d3f;
+            section{
+                display: flex;
+                justify-content: space-between;
+                div:nth-child(1){
+                    position: relative;
+
+                    svg{
+                        position: absolute;
+                        fill: #fff;
+                        top: -1rem;
+                        left: 1rem;
+                        width: 2.5rem;
+                        height: 2.5rem;
+                    }
+                   div{
+                    position: absolute;
+                    left: 3.5rem;
+                   width: 5rem;
+                   p:nth-child(1){
+                    font-size: 1rem;
+                    color: #fff;
+                   }
+                    p:nth-child(2){
+                        font-size: 0.5rem   ;
+                        color: white;
+                    }
+                   }
+                }
+                .food_price{
+                    background-color: #535356;
+                    width: 6rem;
+                    p{
+                        text-align: center;
+                        line-height: 2.5rem;
+                        font-weight: 700;
+                        color: white;
+                    }
                 }
             }
         }
